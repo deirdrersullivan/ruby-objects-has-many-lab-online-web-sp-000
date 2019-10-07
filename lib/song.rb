6891333e388b9ada73913093880f5b40c8b9cb27
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-10-07 04:18:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,18 @@
-class Song 
-attr_accessor :name, :artist
+class Song
+  attr_accessor :name, :artist
 
-@@all = []
+  @@all = []
 
-def initialize(name)
-  @name=name
-  @all << self
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
-def artist_name
+  def artist_name
     artist.name if artist
-end
-
-
+  end
 end
