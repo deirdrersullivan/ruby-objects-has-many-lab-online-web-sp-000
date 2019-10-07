1dc114cc9f015aec925244eb3958237924ce8871
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-10-07 03:49:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,31 @@
-class Artist 
+class Artist
+  attr_accessor :name
 
-attr_accessor :name
+  @@all = []
 
-@@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def initiatlize(name)
-  @name= name
-  @@all << self
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
 end
-
-def self.all
-  @@all
-end
-
-def songs
-  Song.all.select {|song| song.artist == self}
-end
-
-def add_song(song)
-  song.artist= self
-end
-
-def add_song_by_name(name)
-  song = Song.new(name)
-  song.artist = self
-end
-
-def song_count
-  Song.all.count 
-end
-
-end 
-  
